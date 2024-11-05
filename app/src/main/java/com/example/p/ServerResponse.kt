9e0ac552407e7cd41c5832c23daac5b6e9984e09
KUJ <95325650+KUJ1031@users.noreply.github.com>
2024-11-05 16:26:15 +0900

    20241105 버튼 변경 완료
    - 버튼 위젯 디자인 변경 완료

--- app/src/main/java/com/example/p/ServerResponse.kt
+++ app/src/main/java/com/example/p/ServerResponse.kt
@@ -9,7 +9,8 @@ data class SensorData(
     val NH4: String,
     val Acetona: String,
     val temperature: String,
-    val humidity: String
+    val humidity: String,
+    val deviceId: String  // 추가된 deviceId 필드
 )
 
 data class ServerResponse(
